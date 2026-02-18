4119d16e456a039ac7b48fdfb6100e01a28b7564
sc74 <scarlin74@gmail.com>
2026-02-18 14:50:01 +0100

    disables compression

--- scripts/2.0.6.sh
+++ scripts/2.0.6.sh
@@ -3,6 +3,9 @@
 # Add drift file for chrony
 sudo sed -i '$a\driftfile /var/lib/chrony/drift' /etc/chrony.conf
 
-# Xrdp & network
+# Xrdp
 sudo sed -i 's|#    Option "AutoAddDevices" "off"|    Option "AutoAddDevices" "off"|g' /etc/X11/xrdp/xorg.conf
+sudo sed -i 's|    Option "DPMS"|    Option "DPMS" "false"|g' /etc/X11/xrdp/xorg.conf
+sudo sed -i 's|bitmap_compression=true|bitmap_compression=false|g' /etc/xrdp/xrdp.ini
+sudo sed -i 's|bulk_compression=true|bulk_compression=false|g' /etc/xrdp/xrdp.ini
 sudo cp /home/astronaut/.astroarch/configs/99-sysctl.conf /etc/sysctl.d
